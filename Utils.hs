d292aefe47346644ab99e41e1b1d5887511c36e2
miyamok <58558301+miyamok@users.noreply.github.com>
2023-11-13 11:33:55 +0900

    comment added

--- Utils.hs
+++ Utils.hs
@@ -1,10 +1,12 @@
 module Utils where
 import Data.List
 
+-- returns only duplicated elements
 doubles :: Eq a => [a] -> [a]
 doubles [] = []
 doubles (x:xs) = if x `elem` xs then x:doubles (deleteAll x xs) else doubles xs
 
+-- deletes all occurrences of the specified element in the list
 deleteAll :: Eq a => a -> [a] -> [a]
 deleteAll _ [] = []
 deleteAll x xs = if length xs' == length xs then xs else deleteAll x xs'
