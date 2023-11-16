a739b3193dac51f42fa996bf659c451ff90d54f0
miyamok <58558301+miyamok@users.noreply.github.com>
2023-11-16 19:49:04 +0900

    test script concerning variable binding

---
+++ test/VariableBinding.hs
@@ -0,0 +1,25 @@
+module Test.VariableBinding where
+
+-- test script for variable binding
+--import Main
+import Syntax
+import Parser
+import PrettyPrint
+f1 = pf "all x ex y R(h(x, y), g(z))"
+
+t1 = pt "f(x)"
+
+f2 = termSubstitutionInFormula (Var "z" (-1)) t1 f1
+
+f1str = prettyPrintFormula f2
+
+
+compr1 = Compr [Var "z" (-1)] f1
+
+t2 = pt "h1(y, x)"
+f3 = comprehensionAndTermsToFormula compr1 [t2]
+
+main :: IO ()
+main = do putStrLn $ prettyPrintFormula f1
+          putStrLn f1str
+          putStrLn $ prettyPrintFormula f3
